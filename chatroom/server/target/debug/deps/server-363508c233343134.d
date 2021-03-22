21cff3d37d855ecffdcf85434935a5a2fbddd465
KuangjuX <qcx@tju.edu.cn>
2021-03-21 23:52:12 -0700

    update chatroom

--- chatroom/server/target/debug/deps/server-363508c233343134.d
+++
@@ -1,5 +0,0 @@
-/home/kuangjux/Network-Lab/chatroom/server/target/debug/deps/server-363508c233343134: src/main.rs
-
-/home/kuangjux/Network-Lab/chatroom/server/target/debug/deps/server-363508c233343134.d: src/main.rs
-
-src/main.rs:
--- chatroom/server/target/debug/server.d
+++
@@ -1,1 +0,0 @@
-/home/kuangjux/Network-Lab/chatroom/server/target/debug/server: /home/kuangjux/Network-Lab/chatroom/server/src/main.rs
