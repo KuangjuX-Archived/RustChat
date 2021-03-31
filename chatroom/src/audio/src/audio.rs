ed6c0681c776a12a8c1724ff55a5554262ef8863
KuangjuX <qcx@tju.edu.cn>
2021-03-30 20:27:20 -0700

    fix audio

--- chatroom/src/audio/src/audio.rs
+++ chatroom/src/audio/src/audio.rs
@@ -22,7 +22,7 @@ impl Audio {
         pcm.hw_params(&hwp).unwrap();
     }
 
-    pub fn read(pcm: &PCM) -> [i16; 1024]{
+    pub fn read(pcm: &PCM) -> [i16; 44100*10]{
         let io = pcm.io_i16().unwrap();
 
         // Make sure we don't start the stream too early
@@ -31,13 +31,15 @@ impl Audio {
         swp.set_start_threshold(hwp.get_buffer_size().unwrap() - hwp.get_period_size().unwrap()).unwrap();
         pcm.sw_params(&swp).unwrap();
 
-        let mut buf = [0i16; 1024];
+        let mut buf = [0i16; 44100*10];
         
-        for _ in 0..5*44100/1024 {
-            assert_eq!(io.readi(&mut buf[..]).unwrap(), 1024);
-        }
+        // for _ in 0..5*44100/1024 {
+        //     assert_eq!(io.readi(&mut buf[..]).unwrap(), 1024);
+        // }
+        io.readi(&mut buf[..]).unwrap();
 
         println!("Sound Capture: {:?}", buf);
+        // io.readi(&mut buf).unwrap();
 
 
         // In case the buffer was larger than 5 seconds, start the stream manually.
@@ -47,7 +49,7 @@ impl Audio {
         buf
     }
 
-    pub fn write(pcm: &PCM, buf: [i16;1024]){
+    pub fn write(pcm: &PCM, buf: [i16;44100*10]){
         // println!("Receive Sound: {:?}", buf);
         let io = pcm.io_i16().unwrap();
 
@@ -59,9 +61,10 @@ impl Audio {
 
 
         // Play it back for 5 seconds.
-        for _ in 0..5*44100/1024 {
-            assert_eq!(io.writei(&buf[..]).unwrap(), 1024);
-        }
+        // for _ in 0..5*44100/1024 {
+        //     assert_eq!(io.writei(&buf[..]).unwrap(), 1024);
+        // }
+        io.writei(&buf[..]).unwrap();
 
         // In case the buffer was larger than 5 seconds, start the stream manually.
         if pcm.state() != State::Running { pcm.start().unwrap() };
